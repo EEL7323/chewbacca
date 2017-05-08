13bfed50540c31aaf6673ae9b168be3631cdcc12
vini-fern <viniciusfernandesdba@gmail.com>
2017-05-08 20:26:30 -0300

    Add files via upload
    App c++ RU_1.0

---
+++ RU_1/mainwindow.h
@@ -0,0 +1,36 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_actionbuttom_triggered();
+
+    void on_actionText_changed();
+
+    void on_Mat_ok_clicked();
+
+    void on_usuario_text_cursorPositionChanged(int arg1, int arg2);
+
+    void on_passe_text_cursorPositionChanged(int arg1, int arg2);
+
+    void on_usuario_text_editingFinished();
+
+private:
+    Ui::MainWindow *ui;
+
+};
+
+#endif // MAINWINDOW_H
