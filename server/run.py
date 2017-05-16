89b439ca4d1bfe6d3d8aae532f98c22ccf0e1e93
alucassch <alucassch@gmail.com>
2017-05-16 18:39:50 +0000

    server: adiciona opcao de host ao inicilizar o servidor para testes além de localhost

--- server/run.py
+++ server/run.py
@@ -1,4 +1,14 @@
 #!flask/bin/python
-
+import argparse
 from app import app
-app.run(debug=True)
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Inicia o servidor')
+
+	parser.add_argument('--host', type=str, action='store',
+						help='Server Host', default='127.0.0.1')
+
+	args = parser.parse_args()
+
+	app.run(host=args.host,debug=True)
