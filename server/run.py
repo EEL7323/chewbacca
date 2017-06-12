d7df962b24d35b8b2031ed0375b2d6e0742df50b
André Schlichting <andrelucas@linse.ufsc.br>
2017-06-12 18:37:37 -0300

    adiciona suporte a unicode nas requisicoes

--- server/run.py
+++ server/run.py
@@ -1,9 +1,11 @@
 #!flask/bin/python
 import argparse
 from app import app
+import sys
 
 if __name__ == '__main__':
-
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
 	parser = argparse.ArgumentParser(description='Inicia o servidor')
 
 	parser.add_argument('--host', type=str, action='store',
@@ -12,5 +14,5 @@ if __name__ == '__main__':
 						help='Server Port', default=5000)
 
 	args = parser.parse_args()
-
+	
 	app.run(host=args.host,port=args.port,debug=True)
